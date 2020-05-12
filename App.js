2efb15141fa3416b78f42539bfc20c2f34a06c3c
skywindzz <skywindzz@yahoo.com>
2020-05-12 00:25:17 -0700

    bookey

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import MapView from 'react-native-maps';
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Right,
+  Icon,
+  Title,
+  Text,
+  View,
+} from 'native-base';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { StyleSheet, Dimensions } from 'react-native';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <Container>
+        <Header>
+          <Left />
+          <Body>
+            <Title>Bookey App</Title>
+            <View style={styles.container}>
+              <MapView style={styles.mapStyle} />
+            </View>
+          </Body>
+          <Right />
+        </Header>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +62,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
